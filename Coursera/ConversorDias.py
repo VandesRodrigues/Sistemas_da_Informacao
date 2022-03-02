4f64f4b05863f8b523da0d04a0c905f2cd106261
Vandes Rodrigues <vandes@minesoft.com.br>
2022-03-02 17:05:39 -0300

    Inserção Exercicio novo

--- Coursera/ConversorDias.py
+++ Coursera/ConversorDias.py
@@ -1,11 +1,12 @@
 Segundos = int(input("Por favor, entre com o numero de segundos que deseja converter: "))
-Dias = Segundos // 24
-Segundos = Segundos % 24
 
-Horas = Segundos// (60*60)
-Segundos = Segundos % (60*60)
+Dias = Segundos // 86400 # ((60*60)*24) Segundos*Minutos*Hooras 
+Resto = Segundos % 86400
 
-Minutos = Segundos // 60
+Horas = Resto // 3600 # (60*60) Segundos*Minutos
+Resto = Segundos % 3600
+
+Minutos = Resto // 60 # Segundos
 Segundos = Segundos % 60
 
 print(Dias,"dias,",Horas,"horas,",Minutos, "minutos,",Segundos,"segundos.")
